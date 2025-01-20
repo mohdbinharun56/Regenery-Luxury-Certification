f4e5d7122dba990e4342094929091bc912c75cdf
mohdbinharun56 <arahaman461@gmail.com>
2025-01-20 11:29:51 +0600

    added footer section

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,3 +3,6 @@ fetch('/includes/header.html').then(res=>res.text()).then(data=>document.getElem
 
 // display application-status-form to fetch data
 fetch('/application-status.html').then(res=>res.text()).then(data=>document.getElementById('application-status-form').innerHTML=data);
+
+// display footer 
+fetch('/includes/footer.html').then(res=>res.text()).then(data=>document.getElementById('footer').innerHTML=data)
