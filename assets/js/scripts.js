87e1570605f67bc5ed232e730f4cdfa6fa18a7f1
mohdbinharun56 <arahaman461@gmail.com>
2025-01-20 11:13:02 +0600

    added application-status.html

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,2 +1,5 @@
 // display header to fetch
 fetch('/includes/header.html').then(res=>res.text()).then(data=>document.getElementById('header').innerHTML=data) 
+
+// display application-status-form to fetch data
+fetch('/application-status.html').then(res=>res.text()).then(data=>document.getElementById('application-status-form').innerHTML=data);
